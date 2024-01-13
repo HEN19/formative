db3a0c1b72a912575ca69fc6dc8070ee6702fb19
HEN19 <96034033+HEN19@users.noreply.github.com>
2024-01-13 23:35:00 +0700

    Add files via upload

--- New folder/formative/Employee.java
+++ New folder/formative/Employee.java
@@ -1,7 +1,8 @@
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -9,43 +10,108 @@ public class Employee extends Thread {
     private String name;
     private int paycheck;
     private String startDate;
+    private String religion;
+    private StringBuilder outputData;
 
-    public Employee(String name, int paycheck, String startDate) {
+    public Employee(String name, int paycheck, String startDate, String religion) {
         this.name = name;
         this.paycheck = paycheck;
         this.startDate = startDate;
+        this.religion = religion;
+        this.outputData = new StringBuilder();
+    }
+
+    public String getOutputData() {
+        return outputData.toString();
     }
 
     @Override
     public void run() {
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("d MMMM yyyy");
+        SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(name + ".txt"))) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
-            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(inputDateFormat.parse(startDate));
 
-            try {
-                for (int months = 0; months < 12; months++) {
-                    calendar.setTime((dateFormat.parse(startDate)));
-                    calendar.add(Calendar.MONTH, months);
+            for (int months = 0; months < 12; months++) {
+                int sumPaycheck = paycheck;
 
-                    int sumPaycheck = paycheck;
-                    if (months == 4 || months == 12) {
-                        sumPaycheck += paycheck / 12 * (months + 1);
-                    }
-                    String line = dateFormat.format((calendar.getTime()) + " - " + sumPaycheck + " = Montlhy");
-                    writer.write(line);
+                // Check for THR based on religion
+                if ("Islam".equalsIgnoreCase(religion) && months == 3) {
+                    // THR two weeks before 20 April
+                    calendar.set(Calendar.MONTH, Calendar.APRIL);
+                    calendar.set(Calendar.DAY_OF_MONTH, 20);
+                    calendar.add(Calendar.DATE, -14);
+
+                    int thrPaycheck = paycheck / 12 * (months + 1); // THR calculation
+                    sumPaycheck += thrPaycheck;
+
+                    // Format the THR paycheck
+                    String thrLine = name + ": THR Paycheck - " + decimalFormat.format(thrPaycheck) +
+                            " on " + outputDateFormat.format(calendar.getTime());
+                    outputData.append(thrLine).append("\n");
+
+                    writer.write(thrLine);
                     writer.newLine();
+
+                    System.out.println(thrLine);
                 }
 
-            } catch (Exception e) {
-                // TODO: handle exception
+                // Check for Christmas paycheck based on religion
+                if ("Christianity".equalsIgnoreCase(religion) && months == 11) {
+                    // Christmas paycheck two weeks before Christmas Eve (December 24th)
+                    calendar.add(Calendar.DATE, -14);
+                    int christmasPaycheck = paycheck / 12 * (months + 1); // Christmas paycheck calculation
+                    sumPaycheck += christmasPaycheck;
 
-                e.getMessage();
+                    // Format the Christmas paycheck
+                    String christmasLine = name + ": Christmas Paycheck - " + decimalFormat.format(christmasPaycheck) +
+                            " on " + outputDateFormat.format(calendar.getTime());
+                    outputData.append(christmasLine).append("\n");
+
+                    writer.write(christmasLine);
+                    writer.newLine();
+
+                    System.out.println(christmasLine);
+                }
+
+                // For Hindu and Buddhist employees, provide THR in the 12th month
+                if (("Hindu".equalsIgnoreCase(religion) || "Buddhist".equalsIgnoreCase(religion)) && months == 11) {
+                    // THR in the 12th month
+                    calendar.add(Calendar.MONTH, 1);
+                    int thrPaycheck = paycheck / 12 * (months + 1); // THR calculation
+                    sumPaycheck += thrPaycheck;
+
+                    // Format the THR paycheck
+                    String thrLine = name + ": THR Paycheck - " + decimalFormat.format(thrPaycheck) +
+                            " on " + outputDateFormat.format(calendar.getTime());
+                    outputData.append(thrLine).append("\n");
+
+                    writer.write(thrLine);
+                    writer.newLine();
+
+                    System.out.println(thrLine);
+                }
+
+                // Format the sumPaycheck for the current month
+                String line = name + ": " + outputDateFormat.format(calendar.getTime()) + " - " +
+                        decimalFormat.format(sumPaycheck) + " = Monthly";
+                outputData.append(line).append("\n");
+
+                writer.write(line);
+                writer.newLine();
+
+                System.out.println(line);
+
+                // Move to the next month
+                calendar.add(Calendar.MONTH, 1);
             }
 
             System.out.println("File " + name + ".txt berhasil dibuat");
-        } catch (IOException e) {
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
     }
-
 }
